8f995f0ad90b299db108d0f23617407c3ab4226f
MrHate <dgyhateyou@sjtu.edu.cn>
2020-02-14 17:46:18 +0800

    impl basic arithmetic with lambda

--- eval.hpp
+++ eval.hpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <vector>
 #include <map>
+#include <any>
 
 #include "types.hpp"
 
@@ -16,16 +17,39 @@ namespace {
 using namespace LISP;
 
 class Env {
-	typedef std::map<std::string, Exp*> Dict;
+	using Func = std::function<Atom*(const List&)>;
+	using Dict = std::map<std::string, Func>;
+
 	std::vector<Dict> name2exp;
+
+	explicit Env() {
+		Dict builtins;
+		builtins["+"] =  [](const List& list) { return new NumAtom(dynamic_cast<const NumAtom*>(eval(list[1]))->val +  dynamic_cast<const NumAtom*>(eval(list[2]))->val); };
+		builtins["-"] =  [](const List& list) { return new NumAtom(dynamic_cast<const NumAtom*>(eval(list[1]))->val -  dynamic_cast<const NumAtom*>(eval(list[2]))->val); };
+		builtins["*"] =  [](const List& list) { return new NumAtom(dynamic_cast<const NumAtom*>(eval(list[1]))->val *  dynamic_cast<const NumAtom*>(eval(list[2]))->val); };
+		builtins["/"] =  [](const List& list) { return new NumAtom(dynamic_cast<const NumAtom*>(eval(list[1]))->val /  dynamic_cast<const NumAtom*>(eval(list[2]))->val); };
+		builtins[">"] =  [](const List& list) { return new NumAtom(dynamic_cast<const NumAtom*>(eval(list[1]))->val >  dynamic_cast<const NumAtom*>(eval(list[2]))->val); };
+		builtins["<"] =  [](const List& list) { return new NumAtom(dynamic_cast<const NumAtom*>(eval(list[1]))->val <  dynamic_cast<const NumAtom*>(eval(list[2]))->val); };
+		builtins[">="] = [](const List& list) { return new NumAtom(dynamic_cast<const NumAtom*>(eval(list[1]))->val >= dynamic_cast<const NumAtom*>(eval(list[2]))->val); };
+		builtins["<="] = [](const List& list) { return new NumAtom(dynamic_cast<const NumAtom*>(eval(list[1]))->val <= dynamic_cast<const NumAtom*>(eval(list[2]))->val); };
+		builtins["="] =  [](const List& list) { return new NumAtom(dynamic_cast<const NumAtom*>(eval(list[1]))->val == dynamic_cast<const NumAtom*>(eval(list[2]))->val); };
+
+		name2exp.push_back(builtins);
+	}
+
 public:
-	void enter(std::string name, Exp* e) { name2exp.back()[name] = e; }
-	void beginScope() { name2exp.push_back(Dict()); }
-	void endScope() { assert(name2exp.size() > 1); name2exp.resize(name2exp.size() - 1); }
-	Exp* lookup(std::string name) const {
+	static Env& getEnv() {
+		static Env _env;
+		return _env;
+	}
+
+	//void enter(std::string name, Exp* e) { name2exp.back()[name] = e; }
+	//void beginScope() { name2exp.push_back(Dict()); }
+	//void endScope() { assert(name2exp.size() > 1); name2exp.resize(name2exp.size() - 1); }
+	const Func& lookup(std::string name) const {
 		for(auto i = name2exp.rbegin(); i != name2exp.rend(); ++i)
 			if(i->count(name)) return i->at(name);
-		return nullptr;
+		assert(0);
 	}
 };
 
@@ -40,28 +64,7 @@ void eval_define(const List& list) {
 }
 
 const Atom* eval_proc(const std::string oper, const List& list) {
-	if(oper == "+") {
-		assert(list.size() == 3);
-		const NumAtom *lhs = dynamic_cast<const NumAtom*>(eval(list[1])), *rhs = dynamic_cast<const NumAtom*>(eval(list[2]));
-		assert(lhs && rhs);
-		return new NumAtom(lhs->val + rhs->val);
-	}
-	else if(oper == "-") {
-		assert(list.size() == 3);
-		const NumAtom *lhs = dynamic_cast<const NumAtom*>(eval(list[1])), *rhs = dynamic_cast<const NumAtom*>(eval(list[2]));
-		return new NumAtom(lhs->val - rhs->val);
-	}
-	else if(oper == "*") {
-		assert(list.size() == 3);
-		const NumAtom *lhs = dynamic_cast<const NumAtom*>(eval(list[1])), *rhs = dynamic_cast<const NumAtom*>(eval(list[2]));
-		return new NumAtom(lhs->val * rhs->val);
-	}
-	else if(oper == "/") {
-		assert(list.size() == 3);
-		const NumAtom *lhs = dynamic_cast<const NumAtom*>(eval(list[1])), *rhs = dynamic_cast<const NumAtom*>(eval(list[2]));
-		return new NumAtom(lhs->val / rhs->val);
-	}
-	return nullptr;
+	return Env::getEnv().lookup(oper)(list);
 }
 
 } // anonymous namespace
